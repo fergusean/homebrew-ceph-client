ce0c9eb9ce973c3326e1490613442620eba6f03c
Byron Ibarra <bibarrav@hotmail.com>
2024-10-23 17:26:35 -0300

    Update ceph-client.rb

--- ceph-client.rb
+++ ceph-client.rb
@@ -13,7 +13,7 @@ class CephClient < Formula
 
   # depends_on "osxfuse"
   depends_on "boost@1.76"
-  depends_on "cython@0.29.37"
+  depends_on "cython<3.0.0"
   depends_on "openssl" => :build
   depends_on "cmake" => :build
   depends_on "ninja" => :build
